d1fa0fd0c3a6750234b85bb2d86876e866d595d0
Aaron Bengochea <71156554+aaronbengochea@users.noreply.github.com>
2024-04-30 23:24:03 -0400

    adding notes

--- front/src/components/neighborsFeed.jsx
+++ front/src/components/neighborsFeed.jsx
@@ -6,7 +6,10 @@ function NeighborsFeed() {
     <div>
       <Header/>
       <h1>Neighbors Feed</h1>
-      <p>This is the Neighbors feed.</p>
+      <p>This is the Neighbors feed.
+
+        Will update tomorrow.
+      </p>
     </div>
   );
 }
